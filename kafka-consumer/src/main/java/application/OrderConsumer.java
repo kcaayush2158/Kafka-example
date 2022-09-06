dea3604cc8c5bb35636846d2b52f680df395b9ee
Aayush <Aayushkc2158@gmail.com>
2022-09-06 22:20:43 +0545

    Producer & Consumer communication

--- kafka-consumer/src/main/java/application/OrderConsumer.java
+++ kafka-consumer/src/main/java/application/OrderConsumer.java
@@ -1,8 +1,11 @@
 package application;
 
+import application.custome.deserializer.OrderDeserializer;
+import application.model.Order;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
 import java.util.Collections;
@@ -11,21 +14,23 @@ import java.util.Properties;
 public class OrderConsumer {
     public static void main(String[] args) {
 
-        Properties pros = new Properties();
-        pros.setProperty("bootstrap.servers","192.168.1.64:9092");
-        pros.setProperty("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
-        pros.setProperty("value.deserializer","org.apache.kafka.common.serialization.IntegerDeserializer");
-        pros.setProperty("group.id","OrderGroup");
+            Properties pros = new Properties();
+            pros.setProperty("bootstrap.servers","192.168.1.64:9092");
+            pros.setProperty("key.deserializer", StringDeserializer.class.getName());
+            pros.setProperty("value.deserializer", OrderDeserializer.class.getName());
+            pros.setProperty("group.id","OrderGroup");
+            //synchronous way
+            KafkaConsumer<String, Order> consumer = new KafkaConsumer<String, Order>(pros);
+            consumer.subscribe(Collections.singleton("orderCSTopic"));
 
-        //synchronous way
-        KafkaConsumer<String,Integer> consumer = new KafkaConsumer<String, Integer>(pros);
-        consumer.subscribe(Collections.singleton("OrderTopic"));
-
-        ConsumerRecords<String,Integer> orders = consumer.poll(Duration.ofSeconds(20));
-        for(ConsumerRecord<String,Integer> order :orders){
-                System.out.println("Product Name : " + order.key());
-                System.out.println("Quantity :"+order.value());
-        }
+            ConsumerRecords<String,Order> consumerRecords = consumer.poll(Duration.ofSeconds(20));
+            for(ConsumerRecord<String,Order> record :consumerRecords){
+                String consumerName = record.key();
+                Order order= record.value();
+                    System.out.println("Product Name : " + order.getProduct());
+                    System.out.println("Quantity :"+ order.getQuantity());
+            }
+            consumer.close();
 
     }
 }
